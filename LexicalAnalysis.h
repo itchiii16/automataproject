ec333799a6bb0ed36c0badba212f37c637bba001
RinJ07 <127010088+RinJ07@users.noreply.github.com>
2025-12-20 05:08:10 +0800

    Refactor LexicalAnalysis.h header guard and includes

--- LexicalAnalysis.h
+++ LexicalAnalysis.h
@@ -1,144 +1,130 @@
-#ifndef AUTOMATATOOLS_H
-#define AUTOMATATOOLS_H
-
+#ifndef LEXICALANALYSIS_H
+#define LEXICALANALYSIS_H
 
 #include <QWidget>
-#include <QLabel>
-#include <QTextEdit>
-#include <QPushButton>
-#include <QTableWidget>
-#include <QGraphicsView>
 #include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QGraphicsEllipseItem>
-#include <QGraphicsPathItem>
-#include <QGraphicsPolygonItem>
-#include <QGraphicsTextItem>
-#include <QTimer>
 #include <QMap>
-#include <QSet>
-#include <QtMath>
+#include <QList>
+#include <QTimer>
+#include <QLabel>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QStringList>
 
-
-// ==========================
-//        DrawHelper
-// ==========================
-class DrawHelper {
-public:
-    static QGraphicsEllipseItem* createState(
-        QGraphicsScene* scene, qreal x, qreal y, qreal size = 30
-    );
-
-
-    static QGraphicsEllipseItem* createFinalState(
-        QGraphicsScene* scene, qreal x, qreal y, qreal size = 30
-    );
-
-
-    static QList<QGraphicsItem*> createArrow(
-        QGraphicsScene* scene,
-        QGraphicsEllipseItem* from,
-        QGraphicsEllipseItem* to,
-        const QString& label
-    );
-
-
-    static QList<QGraphicsItem*> createCurvedArrow(
-        QGraphicsScene* scene,
-        QGraphicsEllipseItem* from,
-        QGraphicsEllipseItem* to,
-        const QString& label,
-        qreal curveOffset = 70,
-        qreal labelOffsetY = 2000
-    );
-
-
-    static QList<QGraphicsItem*> createSelfLoop(
-        QGraphicsScene* scene,
-        QGraphicsEllipseItem* state,
-        const QString& label
-    );
+// ===============
+// NFA Structures
+// ===============
+struct NFAState {
+    int id;
+    bool isAccept = false;
 };
 
-
-// ==========================
-//      DiagramBuilder
-// ==========================
-struct DiagramElements {
-    QMap<QString, QGraphicsEllipseItem*> states;
-    QMap<QString, QList<QGraphicsItem*>> transitions;
+struct NFATransition {
+    int from;
+    QString symbol; // "ε" or character
+    int to;
 };
 
-
-class DiagramBuilder
-{
-public:
-    static DiagramElements buildExampleDiagram(QGraphicsScene* scene);
+struct NFA {
+    int startState = -1;
+    int acceptState = -1;
+    QList<NFAState> states;
+    QList<NFATransition> transitions;
+    int nextId = 0;
+    int getNextId() { return nextId++; }
 };
 
-
-// ==========================
-//   Animation Step Structure
-// ==========================
+// ===============
+// Animation Step
+// ===============
 struct AnimationStep {
     QString fromState;
     QString toState;
     QString transitionKey;
 };
 
+// ===============
+// DiagramElements
+// ===============
+struct DiagramElements {
+    QMap<QString, QGraphicsEllipseItem*> states;
+    QMap<QString, QList<QGraphicsItem*>> transitions;
+};
 
-// ==========================
-//    LexicalAnalysisTab
-// ==========================
+// ===============
+// DrawHelper
+// ===============
+class DrawHelper
+{
+public:
+    static QGraphicsEllipseItem* createState(QGraphicsScene* scene, qreal x, qreal y, qreal size);
+    static QGraphicsEllipseItem* createFinalState(QGraphicsScene* scene, qreal x, qreal y, qreal size);
+    static QList<QGraphicsItem*> createArrow(QGraphicsScene* scene,
+        QGraphicsEllipseItem* from,
+        QGraphicsEllipseItem* to,
+        const QString& label);
+    static QList<QGraphicsItem*> createCurvedArrow(QGraphicsScene* scene,
+        QGraphicsEllipseItem* from,
+        QGraphicsEllipseItem* to,
+        const QString& label,
+        qreal curveOffset,
+        qreal labelOffsetY);
+    static QList<QGraphicsItem*> createSelfLoop(QGraphicsScene* scene,
+        QGraphicsEllipseItem* state,
+        const QString& label);
+};
+
+// ===============
+// DiagramBuilder
+// ===============
+class DiagramBuilder
+{
+public:
+    static DiagramElements buildDynamicDiagram(QGraphicsScene* scene, const QString& tokenType);
+    
+};
+
+// ===============
+// LexicalAnalysisTab
+// ===============
 class LexicalAnalysisTab : public QWidget
 {
     Q_OBJECT
 
-
 public:
-    LexicalAnalysisTab(QWidget* parent = nullptr);
-    QTableWidget* getTokenizationTable() const { return tokenizationtable; }
-
+    explicit LexicalAnalysisTab(QWidget *parent = nullptr);
 
 signals:
     void tokensReady(const QList<QStringList>& tokens);
-
-
 private slots:
     void runLexicalAnalysis();
     void animateNextStep();
-    void onTokenClicked(QTableWidgetItem* item);  // NEW: Handle token click
-
+    void onTokenClicked(QTableWidgetItem* item);
 
 private:
-    void resetHighlighting();
-    void highlightState(QGraphicsEllipseItem* state);
-    void highlightTransition(const QList<QGraphicsItem*>& items);
-    QList<AnimationStep> getAnimationSteps(const QString& token, const QString& type);
-
-
-    QLabel* userlabel;
     QTextEdit* userinput;
+    QLabel* userlabel;
     QPushButton* run;
-
-
     QLabel* dfa;
     QGraphicsScene* dfaScene;
     QGraphicsView* dfaView;
-
-
     QLabel* tokenlabel;
     QTableWidget* tokenizationtable;
-
-
-    // Animation members
-    DiagramElements diagramElements;
     QTimer* animationTimer;
     QList<QStringList> currentTokens;
     int currentTokenIndex;
-    QList<AnimationStep> currentSteps;
     int currentStepIndex;
-    QString currentAnimatingState;
+    QList<AnimationStep> currentSteps;
+    DiagramElements diagramElements;
+
+    QList<AnimationStep> getAnimationSteps(const QString& token, const QString& type);
+    void resetHighlighting();
+    void highlightState(QGraphicsEllipseItem* state);
+    void highlightTransition(const QList<QGraphicsItem*>& items);
 };
 
-
-#endif
+#endif // LEXICALANALYSIS_H
